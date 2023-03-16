d4d92a40bfa70e80fb258f5d74eeeb28ec53958f
TzipiReiss <116269752+TzipiReiss@users.noreply.github.com>
2023-03-17 00:24:00 +0200

    Update UserModel.cs

--- 2_Services/Models/UserModel.cs
+++ 2_Services/Models/UserModel.cs
@@ -5,23 +5,13 @@ namespace _2_Services.Models
 {
     public class UserModel
     {
-        [Key]
         public int UserId { get; set; }
-        [Required]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
-        [Required]
         public DateTime DateOfBirth { get; set; }
-        [Required]
-        [MaxLength(9)]
-        [MinLength(9)]
         public string IDNumber { get; set; }
-        [Required]
         public Gender Gender { get; set; }
-        [Required]
         public HMO HMO { get; set; }
-
         public List<ChildModel> Children { get; set; } = new List<ChildModel>();
     }
 }
